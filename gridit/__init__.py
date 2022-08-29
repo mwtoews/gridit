265cceeedcfb0afb7dc5d98d912cfdb84fa1a2d9
Mike Taves <mwtoews@gmail.com>
2022-08-29 22:50:25 +1200

    Version and file management via setuptools_scm

--- gridit/__init__.py
+++ gridit/__init__.py
@@ -1,8 +1,12 @@
 """Gridit package."""
-__version__ = "0.1"
 __license__ = "BSD"
 __author__ = "Mike Taves"
 __email__ = "mwtoews@gmail.com"
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = None
 
 __all__ = ["Grid", "GridPolyConv"]
 
