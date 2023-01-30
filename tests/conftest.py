383ae76093796b5c9f6dd5d82aa51f310f62d3df
Mike Taves <mwtoews@gmail.com>
2023-01-30 15:35:07 +1300

    Add pytest report header and fix CLI issue when netCDF4 is not installed

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,13 @@
 import contextlib
 import importlib
 import os
-import pytest
+import re
 import sys
 from importlib import metadata
 from pathlib import Path
 from subprocess import Popen, PIPE
+
+import pytest
 
 try:
     # avoid fiona._env:transform.py:94 Unable to open EPSG support file gcs.csv
@@ -69,3 +71,47 @@ def run_cli(args):
     stderr = stderr.decode()
     returncode = p.returncode
     return stdout, stderr, returncode
+
+
+def pytest_report_header(config):
+    """Header for pytest to show versions of packages."""
+    required = []
+    extra = {}
+    for item in metadata.requires("gridit"):
+        pkg_name = re.findall(r"[a-z0-9_\-]+", item, re.IGNORECASE)[0]
+        if res := re.findall("extra == ['\"](.+)['\"]", item):
+            assert len(res) == 1, item
+            pkg_extra = res[0]
+            if pkg_extra not in extra:
+                extra[pkg_extra] = []
+            extra[pkg_extra].append(pkg_name)
+        else:
+            required.append(pkg_name)
+
+    processed = set()
+    lines = []
+    items = []
+    for name in required:
+        processed.add(name)
+        try:
+            version = metadata.version(name)
+            items.append(f"{name}-{version}")
+        except metadata.PackageNotFoundError:
+            items.append(f"{name} (not found)")
+    lines.append("required packages: " + ", ".join(items))
+    installed = []
+    not_found = []
+    for name in extra["optional"]:
+        if name in processed:
+            continue
+        processed.add(name)
+        try:
+            version = metadata.version(name)
+            installed.append(f"{name}-{version}")
+        except metadata.PackageNotFoundError:
+            not_found.append(name)
+    if installed:
+        lines.append("optional packages: " + ", ".join(installed))
+    if not_found:
+        lines.append("optional packages not found: " + ", ".join(not_found))
+    return "\n".join(lines)
