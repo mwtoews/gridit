dcdaea915abed6103959ee293dec8c1767ba5b8d
Mike Taves <mwtoews@gmail.com>
2023-02-17 14:08:37 +1300

    Import methods from gridit.file

--- gridit/grid.py
+++ gridit/grid.py
@@ -1,14 +1,6 @@
 """Grid class and spatial tools to read array datasets."""
 
 __all__ = ["Grid"]
-
-import numpy as np
-from pathlib import Path
-
-from gridit.display import shorten
-from gridit.logger import get_logger
-from gridit.spatial import is_same_crs
-
 mask_cache = {}
 
 
@@ -41,12 +33,15 @@ class Grid:
     )
     from gridit.cell import cell_geoms, cell_geoseries, cell_geodataframe
     from gridit.modflow import from_modflow, mask_from_modflow
+    from gridit.file import write_raster, write_vector
 
     def __init__(
             self, resolution: float, shape: tuple,
             top_left: tuple = (0.0, 0.0), projection: str = "", logger=None):
 
         if logger is None:
+            from gridit.logger import get_logger
+
             self.logger = get_logger(self.__class__.__name__)
         else:
             self.logger = logger
@@ -110,58 +105,3 @@ class Grid:
         e = -a
         b = d = 0.0
         return Affine(a, b, c, d, e, f)
-
-    def write_raster(self, array, fname: str, driver=None):
-        """Write array to a raster file format.
-
-        Parameters
-        ----------
-        array : array_like
-            Array to write; must have 2-dimensions that match shape.
-        fname : str
-            Output file to write.
-        driver : str or None (default)
-            Raster driver. Default None will determine driver from fname.
-
-        Raises
-        ------
-        ModuleNotFoundError
-            If rasterio is not installed.
-        """
-        from gridit.file import write_raster
-
-        write_raster(self, array=array, fname=fname, driver=driver)
-
-    def write_vector(
-            self, array, fname, attribute, layer=None, driver=None, **kwargs):
-        """Write array to a vector file format.
-
-        Parameters
-        ----------
-        array : array_like
-            Array to write; must have 2-dimensions that match shape.
-        fname : path-like or str
-            Output file to write.
-        attribute : str or list
-            Attribute name. If array is 2D, this is either a str or list with
-            length 1. If array is 3D, this is a list with the same lenght as
-            the first dimension.
-        layer : str or None (default)
-            Vector layer, if implemented by vector driver.
-        driver : str or None (default)
-            Vector driver. Default None will try to determine driver from
-            fname.
-        kwargs : dict, optional
-            Other driver-specific parameters that will be interpreted by the
-            OGR library as layer creation or opening options.
-
-        Raises
-        ------
-        ModuleNotFoundError
-            If fiona and/or shapely is not installed.
-        """
-        from gridit.file import write_vector
-
-        write_vector(
-            self, array=array, fname=fname, attribute=attribute, layer=layer,
-            driver=driver, **kwargs)
