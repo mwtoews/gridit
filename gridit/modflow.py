62c5bd7c1e9ad834db44a6d02a9f7e0668edec3f
Mike Taves <mwtoews@gmail.com>
2024-08-22 15:00:55 +1200

    Feat: create modelgrid without flopy; allow loading from .dis.grb (#39)

--- gridit/modflow.py
+++ gridit/modflow.py
@@ -1,30 +1,197 @@
 """Modflow methods."""
 
-from importlib.util import find_spec
+from dataclasses import dataclass, field
+from os import PathLike
 from pathlib import Path
+from types import SimpleNamespace
+from typing import Any, Optional, Union
 from warnings import catch_warnings, filterwarnings, warn
 
+import numpy as np
+import numpy.typing as npt
+
 from gridit.grid import mask_cache
-from gridit.logger import get_logger
+from gridit.logger import get_logger, logger_factory
 
 
-def get_modflow_model(model, model_name=None, logger=None):
-    """Return model object from str or Path."""
+@dataclass
+class ModelGrid:
+    """Light-weight modelgrid object."""
+
+    delr: npt.NDArray[np.floating] = field(repr=False)
+    delc: npt.NDArray[np.floating] = field(repr=False)
+    domain: npt.NDArray[np.integer] = field(repr=False)
+    xoffset: float = 0.0
+    yoffset: float = 0.0
+    rotation: float = 0.0
+    projection: Optional[str] = None
+    logger: npt.NDArray[np.integer] = field(default_factory=logger_factory, repr=False)
+
+    def __post_init__(self):
+        self.logger = get_logger(self.__class__.__name__)
+
+        self.delr = np.asarray(self.delr)
+        if self.delr.ndim != 1:
+            raise ValueError("model delr is not a 1-d array")
+        delr = self.delr[0]
+        if not (self.delr == delr).all():
+            raise ValueError("model delr is not constant")
+
+        self.delc = np.asarray(self.delc)
+        if self.delc.ndim != 1:
+            raise ValueError("model delc is not a 1-d array")
+        delc = self.delc[0]
+        if not (self.delc == delc).all():
+            raise ValueError("model delc is not constant")
+        elif delr != delc:
+            raise ValueError("model delr and delc are different")
+
+        if self.rotation != 0:
+            self.logger.error("rotated model grids are not supported")
+
+        self.domain = np.asarray(self.domain)
+        if self.domain.ndim == 1:
+            # reshape into 3-d array
+            nrow = self.delc.size
+            ncol = self.delr.size
+            nlay = self.domain.size // (nrow * ncol)
+            self.domain.shape = (nlay, nrow, ncol)
+        elif self.domain.ndim not in (2, 3):
+            raise ValueError("model domain is not a 2- or 3-d array")
+
+    @classmethod
+    def from_modflow(
+        cls,
+        file_or_dir: Union[str, PathLike],
+        model_name: Optional[str] = None,
+        projection: Optional[str] = None,
+        logger=None,
+    ):
+        """Create from MODFLOW file or directory.
+
+        Parameters
+        ----------
+        file_or_dir : str or PathLike
+            Path to a MODFLOW 6 directorty, mfsim.nam, binary grid file
+            (with .dis.grb suffix), or classic MODFLOW NAM file.
+        model_name : str, optional
+            Model name; ignored with binary grid or classic MODFLOW NAM files.
+        projection : optional str, default None
+            WKT coordinate reference system string. If not provided, then try to
+            obtain from model data.
+        logger : logging.Logger, optional
+            Logger to show messages.
+        """
+        model = get_modflow_model(file_or_dir, model_name=model_name)
+        return cls.from_modelgrid(model.modelgrid, projection=projection, logger=logger)
+
+    @classmethod
+    def from_modelgrid(cls, mg: Any, projection: Optional[str] = None, logger=None):
+        """Create from a modelgrid-like object.
+
+        Parameters
+        ----------
+        mg : flopy.discretization.StructuredGrid or similar
+            Object with attributes "delr", "delc", "idomain", "xoffset",
+            "yoffset", "angrot", and "crs".
+        projection : optional str, default None
+            WKT coordinate reference system string. If not provided, then try to
+            obtain from model data.
+        logger : logging.Logger, optional
+            Logger to show messages.
+        """
+        attrs = ["delr", "delc", "idomain", "xoffset", "yoffset", "angrot", "crs"]
+        mg_name = {"idomain": "domain", "angrot": "rotation", "crs": "projection"}
+        kwargs = {mg_name.get(attr, attr): getattr(mg, attr) for attr in attrs}
+        if projection is not None:
+            kwargs["projection"] = projection
+        elif kwargs["projection"] is None:
+            epsg = getattr(mg, "epsg")
+            if isinstance(epsg, int):
+                kwargs["projection"] = f"EPSG:{epsg}"
+            else:
+                kwargs["projection"] = getattr(mg, "proj4")
+        return cls(**kwargs, logger=logger)
+
+    def _grid_kwargs(self):
+        """Returns dict of keyword parameters for Grid.__init__."""
+        return dict(
+            resolution=self.delr[0].item(),
+            shape=self.domain.shape[-2:],
+            top_left=(self.xoffset, self.yoffset + np.sum(self.delc)),
+            projection=self.projection,
+            logger=self.logger,
+        )
+
+    def to_grid(self):
+        """Returns Grid object."""
+        from gridit.grid import Grid
+
+        return Grid(**self._grid_kwargs())
+
+
+def get_modflow_model(
+    file_or_dir: Union[str, PathLike], model_name: Optional[str] = None, logger=None
+):
+    """Return model object from a str or Path to MODFLOW files.
+
+    Parameters
+    ----------
+    file_or_dir : str or PathLike
+        Path to a MODFLOW 6 directorty, mfsim.nam, binary grid file
+        (with .dis.grb suffix), or classic MODFLOW NAM file.
+    model_name : str, optional
+        Model name; ignored with binary grid or classic MODFLOW NAM files.
+    logger : logging.Logger, optional
+        Logger to show messages.
+
+    Raises
+    ------
+    ModuleNotFoundError
+        If flopy is not installed.
+
+    Returns
+    -------
+    Several types of objects, but should have a "modelgrid" attribute.
+
+    """
     import flopy
 
-    if hasattr(model, "xoffset"):
-        from types import SimpleNamespace
+    if hasattr(file_or_dir, "xoffset"):  # this is a modelgrid-like object
+        warn(
+            "getting modflow model from a modelgrid-like object is deprecated; "
+            "use ModelGrid.from_modelgrid(obj) instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return SimpleNamespace(modelgrid=file_or_dir)
+    elif hasattr(file_or_dir, "modelgrid"):  # this is a flopy-like object
+        warn(
+            "getting modflow model from a flopy-like object is deprecated; "
+            "use ModelGrid.from_modelgrid(obj.modelgrid) instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return file_or_dir
+    elif not isinstance(file_or_dir, (str, PathLike)):
+        raise TypeError(f"expected str or PathLike object; found {type(file_or_dir)}")
 
-        tmpobj = SimpleNamespace()
-        tmpobj.modelgrid = model
-        return tmpobj  # dummy object with a modelgrid atrib
-    if hasattr(model, "modelgrid"):
-        return model
-    elif not isinstance(model, (str, Path)):
-        raise TypeError("expected str, Path or model instance")
-    pth = Path(model).resolve()
+    pth = Path(file_or_dir).resolve()
     if not pth.exists():
-        raise ValueError(f"cannot read path '{pth}'")
+        raise FileNotFoundError(f"cannot read path '{pth}'")
+    elif pth.suffixes[-2:] == [".dis", ".grb"]:
+        # Binary grid file
+        if logger is not None:
+            logger.info("reading grid from a binary grid file: %s", pth)
+        if model_name:
+            msg = "ignoring model_name '%s'"
+            args = (model_name,)
+            if logger is None:
+                warn(msg % args, UserWarning, stacklevel=2)
+            else:
+                logger.warning(msg, *args)
+        grb = flopy.mf6.utils.MfGrdFile(pth)
+        return SimpleNamespace(modelgrid=grb.modelgrid)
     elif (pth.is_dir() and (pth / "mfsim.nam").is_file()) or pth.name == "mfsim.nam":
         # MODFLOW 6
         sim_ws = str(pth) if pth.is_dir() else str(pth.parent)
@@ -36,7 +203,7 @@ def get_modflow_model(model, model_name=None, logger=None):
         model_names = list(sim.model_names)
         if model_name is None:
             model_name = model_names[0]
-            msg = "a model name should be specified, "
+            msg = "model_name should be specified, "
             if len(model_names) > 1:
                 msg += "mfsim.nam has %d models (%s); selecting first"
                 args = (len(model_names), model_names)
@@ -55,92 +222,89 @@ def get_modflow_model(model, model_name=None, logger=None):
     elif pth.is_file():  # assume 'classic' MOFLOW file
         with catch_warnings():
             filterwarnings("ignore", category=UserWarning)
-            model = flopy.modflow.Modflow.load(
-                pth.name,
-                model_ws=str(pth.parent),
-                load_only=["dis", "bas6"],
-                check=False,
-                verbose=False,
-                forgive=True,
-            )
+            try:
+                model = flopy.modflow.Modflow.load(
+                    pth.name,
+                    model_ws=str(pth.parent),
+                    load_only=["dis", "bas6"],
+                    check=False,
+                    verbose=False,
+                    forgive=True,
+                )
+            except (ValueError, UnicodeDecodeError):
+                raise ValueError(f"cannot open MODFLOW file '{pth}'")
         return model
     raise ValueError(f"cannot determine how to read MODFLOW model '{pth}'")
 
 
+# imported from Grid class
 @classmethod
 def from_modflow(cls, model, model_name=None, projection=None, logger=None):
     """Create grid information from a MODFLOW model.
 
     Parameters
     ----------
-    model : str, Path, flopy.modflow.Modflow, flopy.mf6.mfmodel.MFModel or flopy.discretization.grid
-        MODFLOW model specified either as a FloPy object, or path to
-        a MODFLOW file.
-    model_name : str or None (default)
-        Needed if MODFLOW 6 simulation has more than one model.
-    projection : str, default None
+    model : str, PathLike, flopy.modflow.Modflow, flopy.mf6.mfmodel.MFModel or flopy.discretization.StructuredGrid
+        Path to a MODFLOW 6 directorty, mfsim.nam, binary grid file
+        (with .dis.grb suffix), classic MODFLOW NAM file,
+        or certain FloPy objects.
+    model_name : str, optional
+        Model name; ignored with binary grid or classic MODFLOW NAM files.
+    projection : str, optional
         Coordinate reference system described as a string either as (e.g.)
         EPSG:2193 or a WKT string. Default None will attempt to obtain
-        this from the model, otherwise it will be "".
+        this from the model, otherwise it will be None.
     logger : logging.Logger, optional
         Logger to show messages.
 
     Raises
     ------
     ModuleNotFoundError
-        If flopy is not installed.
+        Reading from str or PathLike requires flopy.
 
     """  # noqa
-    if find_spec("flopy") is None:
-        raise ModuleNotFoundError("from_modflow requires flopy")
     if logger is None:
         logger = get_logger(cls.__name__)
-    logger.info(
-        "creating from a MODFLOW model: %s",
-        model if isinstance(model, str) else type(model),
-    )
-    mask_cache_key = (repr(model), model_name)
-    model = get_modflow_model(model, model_name, logger)
-    modelgrid = model.modelgrid
-    delr = modelgrid.delr[0]
-    delc = modelgrid.delc[0]
-    if not (modelgrid.delr == delr).all():
-        raise ValueError("model delr is not constant")
-    elif not (modelgrid.delc == delc).all():
-        raise ValueError("model delc is not constant")
-    elif delr != delc:
-        raise ValueError("model delr and delc are different")
-    if modelgrid.angrot != 0:
-        logger.error("rotated model grids are not supported")
-    top_left = (modelgrid.xoffset, modelgrid.yoffset + modelgrid.delc.sum())
-    if projection is None:
-        if isinstance(modelgrid.epsg, int):
-            projection = f"EPSG:{modelgrid.epsg}"
-        elif modelgrid.proj4 is not None:
-            projection = modelgrid.proj4
-        else:
-            projection = ""
-    # also cache mask while we are here
-    domain = modelgrid.idomain
-    mask = (domain == 0).all(0)
-    mask_cache[mask_cache_key] = mask
-    return cls(
-        resolution=delr,
-        shape=modelgrid.top.shape,
-        top_left=top_left,
-        projection=projection,
-        logger=logger,
-    )
+    if isinstance(model, (str, PathLike)):
+        pth = Path(model)
+        logger.info(
+            "creating grid from a MODFLOW model %s: %s",
+            "directory" if pth.is_dir() else "file",
+            model,
+        )
+        # also cache mask while we are here
+        mask_cache_key = (repr(model), model_name)
+        model = get_modflow_model(model, model_name, logger)
+        mg = ModelGrid.from_modelgrid(model.modelgrid)
+        domain = mg.domain
+        mask = (domain == 0).all(0)
+        mask_cache[mask_cache_key] = mask
+    else:
+        logger.info(
+            "creating grid from a MODFLOW model object: %s",
+            type(model),
+        )
+        if hasattr(model, "modelgrid"):
+            modelgrid = model.modelgrid
+        else:  # assume this is a modelgrid object
+            modelgrid = model
+        mg = ModelGrid.from_modelgrid(modelgrid)
+    grid_kwargs = mg._grid_kwargs()
+    if projection is not None:
+        grid_kwargs["projection"] = projection
+    return cls(**grid_kwargs)
 
 
+# imported from Grid class
 def mask_from_modflow(self, model, model_name=None):
     """Return a mask array from a MODFLOW model, based on IBOUND/IDOMAIN.
 
     Parameters
     ----------
-    model : str, Path, flopy.modflow.Modflow, or flopy.mf6.mfmodel.MFModel
-        MODFLOW model specified either as a FloPy object, or path to
-        a MODFLOW file.
+    model : str, PathLike, flopy.modflow.Modflow, flopy.mf6.mfmodel.MFModel or flopy.discretization.StructuredGrid
+        Path to a MODFLOW 6 directorty, mfsim.nam, binary grid file
+        (with .dis.grb suffix), classic MODFLOW NAM file,
+        or certain FloPy objects.
     model_name : str or None (default)
         Needed if MODFLOW 6 simulation has more than one model.
 
@@ -148,16 +312,17 @@ def mask_from_modflow(self, model, model_name=None):
     -------
     np.array
 
-    """
+    """  # noqa
     mask_cache_key = (repr(model), model_name)
     if mask_cache_key in mask_cache:
         self.logger.info("using mask from cache")
         return mask_cache[mask_cache_key]
     model = get_modflow_model(model, model_name, self.logger)
-    if hasattr(model, "bas6"):
-        domain = model.bas6.ibound.array
-    else:
-        domain = model.dis.idomain.array
+    mg = model.modelgrid
+    # reshape idomain into a 3-d array
+    domain = mg.idomain
+    if domain.ndim == 1:
+        domain = domain.reshape(mg.shape)
     mask = (domain == 0).all(0)
     mask_cache[mask_cache_key] = mask
     return mask
--- tests/test_modflow.py
+++ tests/test_modflow.py
@@ -1,5 +1,7 @@
 import logging
+from types import SimpleNamespace
 
+import numpy as np
 import pytest
 
 from gridit import Grid
@@ -15,28 +17,53 @@ def test_get_modflow_model():
 
     from gridit.modflow import get_modflow_model
 
+    # classic MODFLOW
     m = get_modflow_model(modflow_dir / "h.nam")
     assert isinstance(m, flopy.modflow.Modflow)
     assert m.get_package_list() == ["DIS", "BAS6"]
 
-    m = get_modflow_model(m)
+    with pytest.deprecated_call():
+        m = get_modflow_model(m)
     assert isinstance(m, flopy.modflow.Modflow)
 
-    with pytest.warns(UserWarning):
+    # MODFLOW 6
+    with pytest.warns(UserWarning, match="model_name should be specified"):
         m = get_modflow_model(modflow_dir)
-        assert isinstance(m, flopy.mf6.MFModel)
+    assert isinstance(m, flopy.mf6.MFModel)
 
-    with pytest.warns(UserWarning):
+    with pytest.warns(UserWarning, match="model_name should be specified"):
         m = get_modflow_model(modflow_dir / "mfsim.nam")
-        assert isinstance(m, flopy.mf6.MFModel)
+    assert isinstance(m, flopy.mf6.MFModel)
 
     m = get_modflow_model(modflow_dir / "mfsim.nam", "h6")
     assert isinstance(m, flopy.mf6.MFModel)
     assert m.get_package_list() == ["DIS"]
     assert hasattr(m, "tdis")  # check hack
 
-    m = get_modflow_model(m)
+    with pytest.deprecated_call():
+        m = get_modflow_model(m)
     assert isinstance(m, flopy.mf6.MFModel)
+
+    m = get_modflow_model(modflow_dir / "h6.dis.grb")
+    assert isinstance(m, SimpleNamespace)
+    assert hasattr(m, "modelgrid")
+
+    with pytest.warns(UserWarning, match="ignoring model_name"):
+        m = get_modflow_model(modflow_dir / "h6.dis.grb", "unused")
+    assert isinstance(m, SimpleNamespace)
+    assert hasattr(m, "modelgrid")
+
+    # Check exceptions
+    with pytest.raises(TypeError, match="expected str or PathLike object"):
+        get_modflow_model(1)
+    with pytest.raises(FileNotFoundError, match="cannot read path"):
+        get_modflow_model("not-a-path")
+    with pytest.raises(KeyError, match="model name h not found"):
+        get_modflow_model(modflow_dir / "mfsim.nam", "h")
+    with pytest.raises(ValueError, match="cannot open MODFLOW file"):
+        get_modflow_model(modflow_dir / "h.dis")
+    with pytest.raises(ValueError, match="cannot determine how to read MODFLOW model"):
+        get_modflow_model(datadir)
 
 
 @requires_pkg("flopy")
@@ -49,41 +76,45 @@ def test_grid_from_modflow_classic():
 
 @requires_pkg("flopy")
 def test_grid_from_modflow_6(caplog):
-    from gridit.modflow import get_modflow_model
-
+    caplog.set_level(logging.WARNING)
     expected = Grid(1000.0, (18, 17), (1802000.0, 5879000.0))
+    expected_wp = Grid(1000.0, (18, 17), (1802000.0, 5879000.0), projection="EPSG:2193")
 
-    with caplog.at_level(logging.WARNING):
-        grid = Grid.from_modflow(modflow_dir / "mfsim.nam", "h6")
-        assert len(caplog.messages) == 0
-        assert grid == expected
-        assert grid.projection is None
-
-    grid = Grid.from_modflow(modflow_dir / "mfsim.nam", "h6", "EPSG:2193")
-    # assert grid == expected
-    assert grid.projection == "EPSG:2193"
-
-    with caplog.at_level(logging.WARNING):
-        grid = Grid.from_modflow(modflow_dir / "mfsim.nam")
-        assert "a model name should be specified" in caplog.messages[-1]
-        assert grid == expected
-        assert grid.projection is None
-
-    # also rasises logger warning
-    grid = Grid.from_modflow(modflow_dir)
+    grid = Grid.from_modflow(modflow_dir, "h6")
+    assert len(caplog.messages) == 0
     assert grid == expected
 
-    # test with modelgrid object
-    with pytest.warns(UserWarning, match="model name should be specified"):
-        model = get_modflow_model(modflow_dir / "mfsim.nam")
-    grid = Grid.from_modflow(model.modelgrid)
-    assert grid.projection is None
-    assert grid.shape == (18, 17)
-    assert grid.resolution == 1000.0
+    grid = Grid.from_modflow(modflow_dir / "mfsim.nam", "h6", "EPSG:2193")
+    assert len(caplog.messages) == 0
+    assert grid == expected_wp
+
+    caplog.clear()
+    grid = Grid.from_modflow(modflow_dir)
+    assert len(caplog.messages) == 1
+    assert "model_name should be specified" in caplog.messages[-1]
+    assert grid == expected
+
+    caplog.clear()
+    grid = Grid.from_modflow(modflow_dir / "h6.dis.grb")
+    assert len(caplog.messages) == 0
+    assert grid == expected
+
+    caplog.clear()
+    grid = Grid.from_modflow(modflow_dir / "h6.dis.grb", "unused", "EPSG:2193")
+    assert len(caplog.messages) == 1
+    assert "ignoring model_name" in caplog.messages[-1]
+    assert grid == expected_wp
 
 
 @requires_pkg("flopy")
 def test_mask_from_modflow_classic():
+    grid = Grid.from_modflow(modflow_dir / "h.nam")
+    mask = grid.mask_from_modflow(modflow_dir / "h.nam")
+    assert mask.sum() == 128
+
+
+@requires_pkg("flopy")
+def test_mask_from_modflow_6():
     grid = Grid.from_modflow(modflow_dir)
     mask = grid.mask_from_modflow(modflow_dir)
     assert mask.sum() == 128
@@ -92,10 +123,66 @@ def test_mask_from_modflow_classic():
     grid = Grid.from_modflow(modflow_dir)
     mask = grid.mask_from_modflow(modflow_dir / "mfsim.nam", "h6")
     assert mask.sum() == 128
+    mask = grid.mask_from_modflow(modflow_dir / "h6.dis.grb")
+    assert mask.sum() == 128
+
+
+def test_modelgrid():
+    from gridit.modflow import ModelGrid
+
+    # 2D grid
+    mg = ModelGrid(
+        [10, 10],
+        [10, 10, 10],
+        np.ones((2, 3)),
+        xoffset=100,
+        yoffset=200,
+        projection="EPSG:2193",
+    )
+    assert mg.delr.shape == (2,)
+    assert mg.delc.shape == (3,)
+    assert mg.domain.shape == (2, 3)
+    assert mg.to_grid() == Grid(10, (2, 3), (100.0, 230.0), "EPSG:2193")
+    # 3D grid, no projection
+    mg = ModelGrid([10, 10], [10, 10, 10], np.ones((4, 2, 3)), xoffset=100, yoffset=200)
+    assert mg.delr.shape == (2,)
+    assert mg.delc.shape == (3,)
+    assert mg.domain.shape == (4, 2, 3)
+    assert mg.to_grid() == Grid(10, (2, 3), (100.0, 230.0))
 
 
 @requires_pkg("flopy")
-def test_mask_from_modflow_6():
-    grid = Grid.from_modflow(modflow_dir / "h.nam")
-    mask = grid.mask_from_modflow(modflow_dir / "h.nam")
-    assert mask.sum() == 128
+def test_modelgrid_flopy():
+    import flopy
+
+    from gridit.modflow import ModelGrid
+
+    expected = Grid(1000.0, (18, 17), (1802000.0, 5879000.0))
+    expected_wp = Grid(1000.0, (18, 17), (1802000.0, 5879000.0), projection="EPSG:2193")
+
+    m = flopy.modflow.Modflow.load("h.nam", model_ws=modflow_dir, check=False)
+    mg = ModelGrid.from_modelgrid(m.modelgrid)
+    assert mg.to_grid() == expected_wp
+
+    sim = flopy.mf6.MFSimulation.load(sim_ws=modflow_dir)
+    gwf = sim.get_model()
+    mg = ModelGrid.from_modelgrid(gwf.modelgrid)
+    assert mg.to_grid() == expected
+
+    mg = ModelGrid.from_modelgrid(gwf.modelgrid, projection="EPSG:2193")
+    assert mg.to_grid() == expected_wp
+
+    mg = ModelGrid.from_modflow(modflow_dir / "h.nam")
+    assert mg.to_grid() == expected_wp
+
+    mg = ModelGrid.from_modflow(modflow_dir, "h6")
+    assert mg.to_grid() == expected
+
+    mg = ModelGrid.from_modflow(modflow_dir / "mfsim.nam", "h6")
+    assert mg.to_grid() == expected
+
+    mg = ModelGrid.from_modflow(modflow_dir / "h6.dis.grb")
+    assert mg.to_grid() == expected
+
+    mg = ModelGrid.from_modflow(modflow_dir / "mfsim.nam", "h6", "EPSG:2193")
+    assert mg.to_grid() == expected_wp
